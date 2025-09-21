ad9822c618844a89169716ed659a22eae8bf2835
rafi tanujaya <rafibussines@gmail.com>
2025-09-21 21:02:02 +0700

    feat: adding tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 className='text-3xl font-bold'>
+        Saling Tahu
+    </h1>
   </StrictMode>,
 )
